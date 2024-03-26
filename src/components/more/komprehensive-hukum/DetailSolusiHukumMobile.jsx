9b608388030e3b258576e1c4d562358f93437efa
Teguhriyadi <ilham.teguh55@gmail.com>
2024-03-26 10:08:20 +0700

    Modified: Update Content DetailsolusiHukumMobile

--- src/components/more/komprehensive-hukum/DetailSolusiHukumMobile.jsx
+++ src/components/more/komprehensive-hukum/DetailSolusiHukumMobile.jsx
@@ -128,7 +128,7 @@ function DetailSolusiHukumMobile() {
         <div className="responsive-class">
           <div className="res-class">
             <div className="dashboard-container-f" style={{ marginTop: "0px" }}>
-              <div className="container-ketentuan-f">
+              {/* <div className="container-ketentuan-f">
                 <p>
                   Dengan melakukan pemesanan, saya setuju dengan syarat dan
                   ketentuan berikut:{" "}
@@ -139,7 +139,7 @@ function DetailSolusiHukumMobile() {
                     (Klik untuk membaca)
                   </span>
                 </p>
-              </div>
+              </div> */}
               <PaddingPwa padding={15}>
                 <HeaderCheckoutLayanan
                   layanan={getNameLayanan(
